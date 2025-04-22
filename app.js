a619e75e84d0e72d1c4d6a83b42a14ae3a4e90db
Dionatan Henrique <dionatanhs@gmail.com>
2025-04-22 20:54:38 -0300

    Alterando limite para 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 //Criando variavel e atribuindo uma função
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
@@ -15,7 +15,7 @@ function exibirTextoNaTela(tag, texto) {
 function exibirMensagemInicial() {
     //Chamando a função exibirTextoNaTela() e repassando parâmetros
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 exibirMensagemInicial();
@@ -43,7 +43,7 @@ function verificarChute() {
     
 }
 
-//Função apra gerar número aleatório entre 1 e 10
+//Função apra gerar número aleatório entre 1 e 100
 function gerarNumeroAleatorio() {
     let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
     let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
